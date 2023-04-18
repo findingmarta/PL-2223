0589133932c8ac6be1fcbe0f7c352c9e55c1e6ce
Marta Sá <martasa57@hotmail.com>
2023-04-18 17:33:13 +0100

    aaaaaa

--- programa_lex.py
+++ programa_lex.py
@@ -7,14 +7,49 @@ tokens = (
     'CLASS',
     'TEXT',
     'ATTRIBUTE',
+    'IF',
+    'ELSE',
+    'PONTO',
+    'SCRIPT',
+    'VAR'
 )
 
 # Expressões regulares para cada token
-t_TAG = r'[a-zA-Z][a-zA-Z0-9]*'
-t_ID = r'\#[a-zA-Z][a-zA-Z0-9]*'
-t_CLASS = r'\.[a-zA-Z][a-zA-Z0-9]*'
-t_TEXT = r'([^#\.\{\}])+'
-t_ATTRIBUTE = r'\([a-zA-Z][a-zA-Z0-9]*(=("[^"]*"|\'[^\']*\'))?\)'
+def t_IF(t):
+    r'if'
+    return t
+
+def t_ELSE(t):
+    r'else'
+    return t
+
+def t_SCRIPT(t):
+    r'script'
+    return t
+
+def t_VAR(t):
+    r'\w*(?<=\()'
+    return t
+
+def t_TAG(t): 
+    r'[a-zA-Z]\w*'
+    return t
+
+def t_ID(t):
+    r'\w+(?<=\#)'
+    return t
+
+def t_CLASS(t):
+    r'\w+(?<=\.)'
+    return t
+
+def t_TEXT(t):
+    r'(\w+\s*\w*)+'
+    return t
+
+def t_ATTRIBUTE(t):
+    r'\(\w*(=("[^"]*"|\'[^\']*\'))?\)'
+    return t
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -29,3 +64,26 @@ def t_error(t):
     t.lexer.skip(1)
 
 lexer = lex.lex()
+
+pug = '''
+html(lang="en")
+    head
+        title= pageTitle
+        script(type='text/javascript').
+            if (foo) bar(1 + 5)
+    body
+        h1 Pug - node template engine
+        #container.col
+            if youAreUsingPug
+                p You are amazing
+            else
+                p Get on it!
+            p.
+                Pug is a terse and simple templating language with a
+                strong focus on performance and powerful features
+'''
+
+lexer.input(pug)
+
+while tok := lexer.token():
+    print(tok)
