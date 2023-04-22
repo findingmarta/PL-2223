76528d5fc8654215b59f372f517f959955e204ac
Marta Sá <martasa57@hotmail.com>
2023-04-22 23:14:41 +0100

    AAAAAAAAAAAA

--- programa_lex.py
+++ programa_lex.py
@@ -1,78 +1,52 @@
 import ply.lex as lex
 
 states = (
-    ('dentro_ponto','exclusive'),
+    ('dentroPonto','exclusive'),
 )
 
 # Definição dos tokens
 tokens = (
     'ATTRIBUTE',
-    'ATTRIBUTE_NAME',
-    'ATTRIBUTE_VALUE',
     'ATTRIBUTE_THEN',
     'ATTRIBUTE_ELSE',
-    'VIRG',
     'PA',
-    'PF'
+    'PF',
     'TAG',
+    'HASHTAG',
     'ID',
+    'PONTO',
     'CLASS',
     'TEXT',
     'BLOCK_TEXT',
     'IF',
     'ELSE',
-    'PONTO',
-    'SCRIPT',
     'VAR'
 )
 
+
 # Expressões regulares para cada token
+def t_ANY_enter_dentroPonto(t):
+    r'\.(?=\n)'
+    t.lexer.push_state('dentroPonto')
+
+def t_dentroPonto_BLOCK_TEXT(t):   
+    r'[^\t\n]+'
+    return t
+
+def t_HASHTAG(t):
+    r'\#'
+    return t
+
+def t_ID(t):
+    r'(?<=\#)\w+'
+    return t
 
 def t_PONTO(t):
-    r'\.(?=\n)'
-    t.lexer.beggin('dentro_ponto')
+    r'\.'
     return t
 
-def t_dentro_ponto_BLOCK_TEXT(t):
-    tabs += r'^\t+'
-    t.lexer.tabs = tabs.count('\t')
-    
-    print(t.lexer.tabs)
-
-    r'\w[^\n]'
-    return t
-
-def t_ATTRIBUTE_NAME(t):
-    r'\w+(?==)'
-    return t
-
-def t_ATTRIBUTE_VALUE(t):
-    r'\w+(?= )'
-    return t
-
-def t_ATTRIBUTE_THEN(t):
-    r"[a-z' ]+(?=:)"
-
-    return t
-
-def t_ATTRIBUTE_ELSE(t):
-    r"'[a-z']+(?=\))"
-    return t
-
-def t_ATTRIBUTE(t):
-    r'\w+=[^\) ,]*'
-    return t
-
-def t_VIRG(t):
-    r','
-    return t
-
-def t_PA(t):
-    r'\('
-    return t
-
-def t_PF(t):
-    r'\)'
+def t_CLASS(t):
+    r'(?<=\.)\w+'
     return t
 
 def t_IF(t):
@@ -83,30 +57,56 @@ def t_ELSE(t):
     r'else'
     return t
 
-def t_SCRIPT(t):
-    r'script'
+def t_ATTRIBUTE(t):
+    r'(?<=\()[^\)]+'
+    return t
+
+def t_TAG(t): 
+    r'\w+(?=[\(\.\=])|(?<=\t)\w+'
+    return t
+
+
+def t_ATTRIBUTE_THEN(t):
+    r"[a-z' ]+(?=:)"
+    return t
+
+def t_ATTRIBUTE_ELSE(t):
+    r"'[a-z']+(?=\))"
+    return t
+
+def t_PA(t):
+    r'\('
+    return t
+
+def t_PF(t):
+    r'\)'
     return t
 
 def t_VAR(t):
     r'\w*(?<=\()'
     return t
 
-def t_TAG(t): 
-    r'\w+(?<=\t)|\w+(?=\()|^\w+(?=\n)'
+def t_TEXT(t):   
+    r'(?<= )[^\n]+'
     return t
 
-def t_ID(t):
-    r'\w+(?<=\#)'
-    return t
+def t_dentroPonto_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
-def t_CLASS(t):
-    r'\w+(?<=\.)'
-    return t
+    tabs_count = 0
+    for char in t.lexer.lexdata[t.lexer.lexpos:]:
+        if char == ' ':
+            tabs_count += 1
+        elif char == '\t':
+            tabs_count += 4
+        else:
+            break
 
-def t_TEXT(t):
-    r'(\w+\s*\w*)+'
-    return t
+    if tabs_count < t.lexer.tabs:
+        t.lexer.pop_state()
 
+    t.lexer.tabs = tabs_count
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -114,9 +114,9 @@ def t_newline(t):
     t.lexer.lineno += len(t.value)
 
 # Ignora espaços em branco e tabulações
-t_ignore = ' \t'
+t_ANY_ignore = ' \t='
 
-def t_error(t):
+def t_ANY_error(t):
     print('Illegal character: ', t.value[0], ' Line: ', t.lexer.lineno)
     t.lexer.skip(1)
 
@@ -126,20 +126,20 @@ lexer.tabs = 0
 
 pug = '''
 html(lang="en")
-    head
-        title= pageTitle
-        script(type='text/javascript').
-            if (foo) bar(1 + 5)
-    body
-        h1 Pug - node template engine
-        #container.col
-            if youAreUsingPug
-                p You are amazing
-            else
-                p Get on it!
-            p.
-                Pug is a terse and simple templating language with a
-                strong focus on performance and powerful features
+	head
+		title= pageTitle
+			script(type='text/javascript').
+				if (foo) bar(1 + 5)
+	body
+		h1 Pug - node template engine
+			#container.col
+				if youAreUsingPug
+					p You are amazing
+				else
+					p Get on it!
+				p.
+					Pug is a terse and simple templating language with a
+					strong focus on performance and powerful features
 '''
 
 lexer.input(pug)
